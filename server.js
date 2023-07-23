0a4e2137f0cb6c99fbb5fd1758c695f57569df0e
Jesus da silva Tamba <jesustamba2017@gmail.com>
2023-07-23 15:43:44 +0100

    assets styles views server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/assets',express.static('assets'));
+
+app.listen(8085, () => {
+    console.log(`Running on port 8085`);
+})
