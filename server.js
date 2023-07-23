d1b5ce867cb842e3b001f0cb04dffd6f6e51143a
Jesus da silva Tamba <jesustamba2017@gmail.com>
2023-07-23 19:38:05 +0100

    tailwindcss

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/assets',express.static('assets'));
 
+app.get("/", (req,res) => {
+    res.render('index', {});
+})
+
 app.listen(8085, () => {
     console.log(`Running on port 8085`);
 })
